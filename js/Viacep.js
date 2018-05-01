2f688f095ca494b536fb5c85681b3f5e82aeebc2
Marinho Renan <renanmarinh3@hotmail.com>
2018-04-30 22:54:56 -0300

    Commit

---
+++ js/Viacep.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	$('#cep').blur(function(){
+		var cep = $('#cep').val();
+		var url = 'https://viacep.com.br/ws/' +cep+ '/json';
+		$.getJSON(url, function(data){
+			if("error" in data){
+
+			alert("Deu ruim");
+		}
+		$('#rua').val(data.logradouro);
+		$('#bairro').val(data.bairro);
+		$('#cidade').val(data.localidade);
+		$('#uf').val(data.uf);
+
+		})
+	})
+})
